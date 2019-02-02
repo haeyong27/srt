cdcac6e961a2abe838b274b94365d8b7fc170240
haeyong27 <haeyong.hwang27@gmail.com>
2019-02-02 22:35:16 +0900

    add 'is_public' column

--- blog/admin.py
+++ blog/admin.py
@@ -5,5 +5,6 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'created_at']
-    
+    list_display = ['id', 'title', 'created_at', 'is_public']
+    list_display_links = ['title']
+    list_editable = ['is_public']
