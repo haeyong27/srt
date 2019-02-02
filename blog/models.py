12a67e0bc93f1b4e50314a0849c01f6726b1a067
haeyong27 <haeyong.hwang27@gmail.com>
2019-02-02 22:44:28 +0900

    add photo column

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    is_public = models.BooleanField(default = False)
+    is_public = models.BooleanField(default=False)
+    photo = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
