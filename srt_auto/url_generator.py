b7dfec3f3c4cce8c7372a7e37b262dcb1a1ecff2
haeyong27 <haeyong.hwang27@gmail.com>
2019-02-03 17:21:47 +0900

    출발시간 저장

--- srt_auto/url_generator.py
+++ srt_auto/url_generator.py
@@ -1,8 +1,8 @@
-from . import station_num
+import find_station_num
 
 class URL_gen():
     def __init__(self, start='부산', end='동탄', date='20190205', person = 1, dpTime = 0):
-        c = station_num.StationNum()
+        c = find_station_num.StationNum()
         self.station_dic = c.station_num()
         self.url_base = 'https://etk.srail.co.kr/hpg/hra/01/selectScheduleList.do?pageId=TK0101010000'
         self.url = 'dptRsStnCd=0552&arvRsStnCd=0020&stlbTrnClsfCd=05&psgNum=1&seatAttCd=015&isRequest=Y&dptRsStnCdNm=%EB%8F%99%ED%83%84&arvRsStnCdNm=%EB%B6%80%EC%82%B0&dptDt=20190203&dptTm=105900&chtnDvCd=1&psgInfoPerPrnb1=1&psgInfoPerPrnb5=0&psgInfoPerPrnb4=0&psgInfoPerPrnb2=0&psgInfoPerPrnb3=0&locSeatAttCd1=000&rqSeatAttCd1=015&trnGpCd=109'
@@ -41,9 +41,9 @@ class URL_gen():
             d[a] = b
         return d
 
-if __name__ == "__main__":
-    a = URL_gen('부산', '동탄', '20190205')
-    print(a.generate())
+
+a = URL_gen('부산', '동탄', '20190205')
+print(a.generate())
 
 
 
