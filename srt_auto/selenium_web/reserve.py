ff0ef299d83d2e01ac7157f67fd92607c8d69e73
haeyong27 <haeyong.hwang27@gmail.com>
2019-02-03 17:00:23 +0900

    특정조건 만족하는 예약화면 띄우기

--- srt_auto/selenium_web/reserve.py
+++ srt_auto/selenium_web/reserve.py
@@ -21,13 +21,15 @@ class Reserve():
         time.sleep(1)
         tag_pw.submit()
     
-    def get_url(self):
+    def get_reserve_url(self):
         a = url_generator.URL_gen()
         return a.generate()
 
 a = Reserve()
+time.sleep(2)
 a.login('01021843577', 'sphv8401')
-a.driver.get(a.get_url())
+a.driver.get(a.get_reserve_url())
+
 
 
 # url = 'https://etk.srail.co.kr/hpg/hra/01/selectScheduleList.do?pageId=TK0101010000&dptRsStnCd=0020&arvRsStnCd=0552&stlbTrnClsfCd=05&psgNum=1&seatAttCd=015&isRequest=Y&dptRsStnCdNm=부산&arvRsStnCdNm=동탄&dptDt=20190205&dptTm=000000&chtnDvCd=1&psgInfoPerPrnb1=1&psgInfoPerPrnb5=0&psgInfoPerPrnb4=0&psgInfoPerPrnb2=0&psgInfoPerPrnb3=0&locSeatAttCd1=000&rqSeatAttCd1=015&trnGpCd=109'
