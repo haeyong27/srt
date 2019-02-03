845164cba18a47c858ec2d7526b5030f92c9d0fa
haeyong27 <haeyong.hwang27@gmail.com>
2019-02-04 02:02:00 +0900

    test

--- srt_auto/test.py
+++ srt_auto/test.py
@@ -1,9 +1,11 @@
 from . import reserve
 
-a = reserve.Reserve()
-# a.login('01021843577', 'sphv8401')
-a.driver.get(a.get_reserve_url())
 
-time_list = a.find_dpTime()
-for i in time_list:
-    a.button_click(i)
+class Test:
+    def __init__(self, id, pw, dpst, endst, date, startTime, endTime):
+        a = reserve.Reserve()
+        a.login(id, pw)
+        a.driver.get(a.get_reserve_url())
+        time_list = a.find_dpTime()
+        for i in time_list:
+            a.button_click(i)
