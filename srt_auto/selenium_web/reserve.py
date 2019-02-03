1e4aa5664d0dca376e312ef0e8147aa5d68f82c4
haeyong27 <haeyong.hwang27@gmail.com>
2019-02-04 01:23:48 +0900

    오타수정

--- srt_auto/selenium_web/reserve.py
+++ srt_auto/selenium_web/reserve.py
@@ -8,7 +8,7 @@ class Reserve():
         url = 'https://etk.srail.co.kr/cmc/01/selectLoginForm.do?pageId=TK0701000000'
         self.driver.get(url)
         
-        
+
     def login(self, id, pw):
         time.sleep(1)
         tag_category = self.driver.find_element_by_id('srchDvCd3')
@@ -39,5 +39,5 @@ class Reserve():
 
     def button_click(self):
         for i in self.find_dpTime():
-            a = self.driver.find_elements_by_xpath('//a[@onclick="requestReservationInfo(this, 0, \'{}}\', \'1101\', true, false); return false;"]'.format(i))
+            a = self.driver.find_elements_by_xpath('//a[@onclick="requestReservationInfo(this, 0, \'{}\', \'1101\', true, false); return false;"]'.format(i))
             a[0].click()
