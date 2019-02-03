8e7036987fd4845f704729eaa8e7e66df2c522c7
haeyong27 <haeyong.hwang27@gmail.com>
2019-02-03 16:50:27 +0900

    rename (라이브러리네임겹쳐서 이름바꿈)

--- srt_auto/selenium_web/reserve.py
+++ srt_auto/selenium_web/reserve.py
@@ -1,6 +1,6 @@
 import time
 from selenium import webdriver
-
+from .. import url_generator
 
 class Reserve():
     def __init__(self):
@@ -22,10 +22,14 @@ class Reserve():
         tag_pw.submit()
     
     def get_url(self):
-        pass
+        a = url_generator.URL_gen()
+        return a.generate()
 
 a = Reserve()
 a.login('01021843577', 'sphv8401')
+a.driver.get(a.get_url())
+
+
 # url = 'https://etk.srail.co.kr/hpg/hra/01/selectScheduleList.do?pageId=TK0101010000&dptRsStnCd=0020&arvRsStnCd=0552&stlbTrnClsfCd=05&psgNum=1&seatAttCd=015&isRequest=Y&dptRsStnCdNm=부산&arvRsStnCdNm=동탄&dptDt=20190205&dptTm=000000&chtnDvCd=1&psgInfoPerPrnb1=1&psgInfoPerPrnb5=0&psgInfoPerPrnb4=0&psgInfoPerPrnb2=0&psgInfoPerPrnb3=0&locSeatAttCd1=000&rqSeatAttCd1=015&trnGpCd=109'
 # driver.get(url)
 # a = driver.find_elements_by_xpath('//a[@onclick="requestReservationInfo(this, 0, \'2\', \'1101\', true, false); return false;"]')
