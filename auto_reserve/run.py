19e4be1af4d8b512a9202f7e086612e22d630c3a
Haeyong Hwang <41499598+haeyong27@users.noreply.github.com>
2019-03-03 16:39:58 +0900

    Update run.py

--- auto_reserve/run.py
+++ auto_reserve/run.py
@@ -1,16 +1,5 @@
 import reserve
 
-# a = reserve.Reserve('01021843577')
-# a.login('01021843577', 'sphv8401', 3)
-# url = a.get_reserve_url('동탄', '부산', '20190301', 1, 0, 8)
-# a.driver.get(url)
-# time.sleep(1)
-# while(True):
-#     a.find_dpTime_click(8, 20) #몇시 부터 몇시 사이의 표를 예매할건지
-#     time.sleep(1)
-#     if (a.complete()==1): #예매 완료되면 종료
-#         break
-
 class Run():
     def __init__(self, phone, id1, pw, dptStn, arrStn, date1, numAdult, numChild, fromTime, toTime):
         self.phone = phone
@@ -40,5 +29,5 @@ class Run():
         self.res.driver.quit()
 
 if __name__=='__main__':
-    a = Run('01021843577', '01021843577', 'sphv8401', '동탄', '부산', '20190306', 2, 1, '09:59', '14:10')
+    a = Run('11', '01021843577', 'sphv8401', '수서', '부산', '20190306', 1, 0, '09:59', '14:10')
     a.run()
